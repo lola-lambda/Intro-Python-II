6d3e0b670e9b3439b69baed42ba0630e730590c9
Lola <lolahef@gmail.com>
2019-02-12 19:09:09 -0500

    Created Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,9 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+class Room():
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+        self.items = []
+    def print_room(self):
+        print(f"{self.name} {self.desc}")
+    def print_items(self):
+        print(f"available items: {self.items}")
