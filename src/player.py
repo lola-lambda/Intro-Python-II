c647c8d4b00665fc4169ce010e2cade8c5b6a7b9
Lola <lolahef@gmail.com>
2019-02-12 23:51:16 -0500

    Added check inventory, drop item, and take item methods

--- src/player.py
+++ src/player.py
@@ -3,7 +3,19 @@ class Player():
         self.name = name
         self.room = room
         self.items = []
+
     def check_inventory(self):
-        print(f"your inventory includes {self.items}")
+        if len(self.items) > 0:
+            all_items = ", ".join([str(item) for item in self.items])
+        else:
+            all_items = "nothing"
+        print(f"Your inventory contains {all_items}.")
+
+    def drop_item(self, item):
+        self.items = list(filter(lambda i: not i.name is item.name, self.items))
+
+    def take_item(self, item):
+        self.items.append(item)
+
     def move_rooms(self, room):
         self.room = room
