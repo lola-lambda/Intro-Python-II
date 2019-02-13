94329ff870d3726bed438ee82bf11577da175611
Lola <lolahef@gmail.com>
2019-02-12 23:51:41 -0500

    Added print items, give item, and receive item methods

--- src/room.py
+++ src/room.py
@@ -1,9 +1,18 @@
 class Room():
-    def __init__(self, name, desc):
+    def __init__(self, name, desc, items):
         self.name = name
         self.desc = desc
-        self.items = []
-    def print_room(self):
-        print(f"{self.name} {self.desc}")
+        self.items = items
+
     def print_items(self):
-        print(f"available items: {self.items}")
+        if len(self.items) > 0:
+            all_items = ", ".join([str(item) for item in self.items])
+        else:
+            all_items = "nothing"
+        print(f"You look around the {self.name} and see {all_items}.")
+
+    def give_item(self, item):
+        self.items = list(filter(lambda i: not i.name is item.name, self.items))
+    
+    def receive_item(self, item):
+        self.items.append(item)
